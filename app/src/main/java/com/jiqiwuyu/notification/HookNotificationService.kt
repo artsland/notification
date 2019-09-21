be59f1b39a8d8f848b3dd8d04aef55fdf19866df
Max <weiping@diancloud.com>
2019-09-22 00:24:34 +0800

    publish

---
+++ app/src/main/java/com/jiqiwuyu/notification/HookNotificationService.kt
@@ -0,0 +1,75 @@
+package com.jiqiwuyu.notification
+import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import android.os.Bundle;
+import android.util.Log;
+import android.preference.PreferenceManager
+import com.github.kittinunf.fuel.httpPost
+import com.github.kittinunf.fuel.core.extensions.jsonBody
+import com.github.kittinunf.result.Result;
+import org.json.JSONObject;
+import org.json.JSONException
+
+
+class HookNotificationService : NotificationListenerService() {
+
+
+    override fun onListenerConnected() {
+    }
+
+
+    /**
+     * 发送数据到绑定地址
+     */
+    fun postData( url:String, body: JSONObject){
+        val httpAsync = url
+            .httpPost()
+            .jsonBody(body.toString())
+            .responseString { request, response, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        println(ex.message)
+                    }
+                    is Result.Success -> {
+                        val data = result.get()
+                        println(data)
+                    }
+                }
+            }
+
+        httpAsync.join()
+    }
+
+    /**
+     * 监听状态栏通知消息
+     *
+     * Todo:
+     *   - 1. 微信屏蔽了通知服务监听？ 启动微信时，监听不到消息
+     *   - 2. 进程被杀死后，此方法失效？
+     */
+    override  fun onNotificationPosted( sbn: StatusBarNotification  ){
+
+        Log.d("HookNotificationService","onNotificationPosted() called");
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        val url = sharedPref.getString(getString(R.string.url_key), "Not Set").toString()
+        val extras:Bundle = sbn.getNotification().extras;
+        val json = JSONObject()
+        val keys = extras.keySet()
+        json.put("package", sbn.getPackageName());
+        for (key in keys) {
+            try {
+                // json.put(key, bundle.get(key)); see edit below
+                json.put(key, JSONObject.wrap(extras.get(key)))
+            } catch (e: JSONException) {
+                //Handle exception here
+            }
+        }
+
+        // 提交数据请求
+        postData( url, json );
+    }
+
+    override fun onNotificationRemoved(sbn: StatusBarNotification) {
+    }
+}
---
+++ app/src/main/java/com/jiqiwuyu/notification/MainActivity.kt
@@ -0,0 +1,173 @@
+package com.jiqiwuyu.notification
+
+import android.content.ComponentName
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import android.content.ActivityNotFoundException
+import android.content.Context
+import androidx.core.content.ContextCompat
+import android.app.ActivityManager;
+import android.preference.PreferenceManager
+import android.util.Log
+import android.service.notification.NotificationListenerService
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /**
+     * 检查是否有通知访问权限
+     */
+    fun checkPermistion():Boolean {
+
+        val pkgName:String = getPackageName()
+        val setting = Settings.Secure.getString(contentResolver, "enabled_notification_listeners");
+        println("已有通知权限的应用 ${pkgName} ${setting}")
+        if ( !TextUtils.isEmpty(setting) ) {
+            val names = setting.split(":")
+            for( name in names ) {
+                val com = ComponentName.unflattenFromString(name)
+                if ( com != null  ) {
+                    if ( TextUtils.equals(pkgName, com.packageName )) {
+                        return true
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    /**
+     * 打开通知权限配置界面
+     */
+    fun openSetting():Boolean {
+
+        try {
+            val intent: Intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            return true
+        } catch ( e : ActivityNotFoundException ) {
+            Toast.makeText(applicationContext, "系统不支持此方法", Toast.LENGTH_SHORT).show()
+            return false;
+        }
+    }
+
+
+    /**
+     * 检查服务是否正在运行
+     */
+    fun serviceIsRunning():Boolean {
+        val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+            if ("com.jiqiwuyu.notification.HookNotificationService" == service.service.className) {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    /**
+     * 启动服务
+     */
+    fun startServiceClick( view: View ) {
+
+        Log.d("HookManager","startServiceClick() called");
+
+        // 通知读取权限检查
+        if (!this.checkPermistion() ) {
+            this.openSetting()
+            return
+        }
+
+        // 读取接收数据地址
+        val urlText = findViewById<EditText>(R.id.input_url)
+        var url:String = urlText.text.toString()
+        if ( url == "" ) {
+            url = getString(R.string.url_default)
+        }
+
+        // 保存URL地址
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+        with (sharedPref.edit()) {
+            putString(getString(R.string.url_key), url)
+            commit()
+        }
+
+        val setUrl = sharedPref.getString(getString(R.string.url_key), "Not Set")
+        Log.d("HookManager ", "setUrl=${setUrl}");
+
+        if ( !this.serviceIsRunning() ) {
+
+            //  启动服务操作
+            val serviceClass = HookNotificationService::class.java
+            val serviceIntent = Intent(applicationContext, serviceClass)
+            startService(serviceIntent)
+            NotificationListenerService.requestRebind(ComponentName(this, serviceClass))
+
+            if ( !this.serviceIsRunning() ) {
+                Toast.makeText(applicationContext, "服务启动失败", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+        } else {
+            val serviceClass = HookNotificationService::class.java
+            NotificationListenerService.requestRebind(ComponentName(this, serviceClass))
+            Toast.makeText(applicationContext, "服务已经启动", Toast.LENGTH_SHORT).show()
+        }
+
+
+        // 读取状态控制器
+        val statusText = findViewById<TextView>(R.id.text_status)
+        statusText.setText("服务已启动")
+        statusText.setTextColor( ContextCompat.getColor(applicationContext,R.color.colorSuccess))
+
+        // 读取消息通知
+        val messageText = findViewById<TextView>(R.id.text_message)
+        messageText.setText(TextUtils.concat("系统通知将同步推送至:","\n${url}"))
+    }
+
+    /**
+     * 关闭服务
+     */
+    fun stopServiceClick( view: View ) {
+
+        // 关闭服务操作
+        if ( this.serviceIsRunning() ) {
+
+            val serviceClass = HookNotificationService::class.java
+            val serviceIntent = Intent(applicationContext, serviceClass)
+            stopService(serviceIntent)
+
+            if ( this.serviceIsRunning() ) {
+                Toast.makeText(applicationContext, "服务关闭失败", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        } else {
+            Toast.makeText(applicationContext, "服务尚未启动", Toast.LENGTH_SHORT).show()
+        }
+
+        // 读取状态控制器
+        val statusText = findViewById<TextView>(R.id.text_status)
+        statusText.setText("服务未开启")
+        statusText.setTextColor( ContextCompat.getColor(applicationContext,R.color.colorAccent))
+
+        // 读取消息通知
+        val messageText = findViewById<TextView>(R.id.text_message)
+        messageText.setText("")
+
+    }
+}
