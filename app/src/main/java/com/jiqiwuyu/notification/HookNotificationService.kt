6166b4f34848377b944eccf75c0dbac9e3dc4b3a
Max <weiping@diancloud.com>
2019-10-04 15:09:59 +0800

    + 发送状态栏所有通知

--- app/src/main/java/com/jiqiwuyu/notification/HookNotificationService.kt
+++ app/src/main/java/com/jiqiwuyu/notification/HookNotificationService.kt
@@ -7,12 +7,12 @@ import android.preference.PreferenceManager
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.result.Result;
+import org.json.JSONArray
 import org.json.JSONObject;
 import org.json.JSONException
 
 
 class HookNotificationService : NotificationListenerService() {
-
 
     override fun onListenerConnected() {
     }
@@ -50,13 +50,16 @@ class HookNotificationService : NotificationListenerService() {
      */
     override  fun onNotificationPosted( sbn: StatusBarNotification  ){
 
+
         Log.d("HookNotificationService","onNotificationPosted() called");
         val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
         val url = sharedPref.getString(getString(R.string.url_key), "Not Set").toString()
-        val extras:Bundle = sbn.getNotification().extras;
+
+        // 读取触发事件的通知
+        val extras:Bundle = sbn.notification.extras
         val json = JSONObject()
         val keys = extras.keySet()
-        json.put("package", sbn.getPackageName());
+        json.put("package", sbn.packageName)
         for (key in keys) {
             try {
                 // json.put(key, bundle.get(key)); see edit below
@@ -66,8 +69,28 @@ class HookNotificationService : NotificationListenerService() {
             }
         }
 
+        // 读取当前状态栏所有通知
+        val notifications = JSONArray()
+        for ( n in activeNotifications ) {
+            val extras:Bundle = n.notification.extras
+            val obj = JSONObject()
+            val keys = extras.keySet()
+            obj.put("package", n.packageName )
+            for (key in keys) {
+                try {
+                    // json.put(key, bundle.get(key)); see edit below
+                    obj.put(key, JSONObject.wrap(extras.get(key)))
+                } catch (e: JSONException) {
+                    //Handle exception here
+                }
+            }
+            notifications.put(obj)
+        }
+
+        json.put("notifications", notifications )
+
         // 提交数据请求
-        postData( url, json );
+        postData( url, json )
     }
 
     override fun onNotificationRemoved(sbn: StatusBarNotification) {
